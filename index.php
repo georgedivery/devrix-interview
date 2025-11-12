4be16ec7878fd7d70130bdb4580232f25c30bcac
georgedivery <georgedivery@email.com>
2025-11-12 19:40:45 +0200

    init

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<div class="main">
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <section class="section-post">
+        <div class="shell">
+            <div class="section-head">
+                <h2 class="section-title">
+                    <?php the_title(); ?>
+                </h2>
+            </div>
+
+            <div class="section-body">
+                <?php the_content(); ?>
+            </div>
+
+            <div class="section-foot">
+                <a href="<?php echo get_permalink() ?>" class="link-more">
+                    Read more
+                </a>
+            </div>
+        </div>
+    </section>
+    <?php endwhile; endif;  ?>
+</div>
+
+<?php get_footer(); ?>
---
+++ sections.php
@@ -0,0 +1,35 @@
+<?php
+if (!function_exists('have_rows') || !function_exists('get_field')) :
+	?>
+<section>
+    <div class="shell">
+        <h2>
+            Advanced Custom Fields plugin is not active.
+        </h2>
+    </div>
+</section>
+<?php
+	return;
+endif;
+
+$sections_field = get_field('sections');
+
+if (!$sections_field) :
+	?>
+<section>
+    <div class="shell">
+        <h2>
+            No sections created yet
+        </h2>
+    </div>
+</section>
+<?php
+	return;
+endif;
+?>
+
+<?php while (have_rows('sections')) : the_row(); ?>
+<?php if (get_row_layout() === 'hero_section') : ?>
+<?php include 'sections/hero/index.php'; ?>
+<?php endif; ?>
+<?php endwhile; // end "while" flex content?>
